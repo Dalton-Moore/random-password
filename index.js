e939b59175627fbfa6a208d7a526a5db160dd493
Dalton-Moore <162634280+Dalton-Moore@users.noreply.github.com>
2025-03-10 19:57:52 -0600

    checkboxes and number increment
    needs more js, improved checkboxes and created unique button for number increment, still needs sizing fixed and more style all around.

--- index.js
+++ index.js
@@ -1,20 +1,40 @@
-let generateEl = document.getElementById(generate-el)
-let password1El = document.getElementById(password1-el)
-let password2El = document.getElementById(password2-el)
-const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
-"/"];
+let input = document.querySelector(".number-input");
+const min = input.getAttribute("min");
+const max = input.getAttribute("max");
+const step = Number(input.getAttribute("step") || 1);
+var value = Number(input.getAttribute("value") || 0);
+let number = document.querySelector(".number");
+number.innerHTML = value;
 
-let number = 12;
-
-function generatePassword() {
-    let password = "";
-    for (let i = 0; i < password.length; i++) {
-        let randomNumber = Math.floor(Math.random() * characters.length);
+function increase() {
+    if(max) {
+        if (value < max && value + step <= max) {
+            value += step;
+        }
+    } else {
+        value += step;
     }
-    return password;
+    input.style.transform = "rotateY(20deg)";
+    setTimeout(() => {
+        input.style.transform = "rotateY(0deg)";
+    }, 150);
+    number.innerHTML = value
 }
 
-generateEl.addEventListener("click", function() {
-    password1El.textContent = generatePassword();
-    password2El.textContent = generatePassword();
-});
+function decrease() {
+    if(min) {
+        if (value > min && value - step >= min) {
+            value -= step;
+        }
+    } else {
+        value -= step;
+    }
+    input.style.transform = "rotateY(-20deg)";
+    setTimeout(() => {
+        input.style.transform = "rotateY(0deg)";
+    }, 150);
+    number.innerHTML = value
+}
+
+
+
