0546e5f71d1227d9ea1a5c62a3a9337de833e0f8
Dalton-Moore <162634280+Dalton-Moore@users.noreply.github.com>
2025-03-12 16:34:27 -0600

    started random JS and fixed layout some
    worked on the JS function to generate the random password. worked on some spatial issues with the layout.
    
    still needs the JS completed and some more styling.

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const symbols = ["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?","/"];
+const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+
+
+let lengthValue = document.getElementById("lengthValue");
+
+
+
+bigBtn.addEventListener("input", function() {
+    paswordLength = parseInt(bigBtn.value);
+    lengthValue.textContent = passwordLength;
+});
+
+
+
+
+
+
+
+
+
+
 let input = document.querySelector(".number-input");
 const min = input.getAttribute("min");
 const max = input.getAttribute("max");
