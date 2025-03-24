4f524bbcc06fae94303b410cef1abc215c89652e
Dalton-Moore <162634280+Dalton-Moore@users.noreply.github.com>
2025-03-24 10:49:43 -0600

    update for netlify
    update for netlify

--- index.js
+++ index.js
@@ -5,66 +5,13 @@
 
 
 
-// let input = document.querySelector(".number-input");
-// const min = input.getAttribute("min");
-// const max = input.getAttribute("max");
-// const step = Number(input.getAttribute("step") || 1);
-// var value = Number(input.getAttribute("value") || 0);
-// let number = document.querySelector(".number");
-// number.innerHTML = value;
-
-// function increase() {
-//     if(max) {
-//         if (value < max && value + step <= max) {
-//             value += step;
-//         }
-//     } else {
-//         value += step;
-//     }
-//     input.style.transform = "rotateY(20deg)";
-//     setTimeout(() => {
-//         input.style.transform = "rotateY(0deg)";
-//     }, 150);
-//     number.innerHTML = value
-// }
-
-// function decrease() {
-//     if(min) {
-//         if (value > min && value - step >= min) {
-//             value -= step;
-//         }
-//     } else {
-//         value -= step;
-//     }
-//     input.style.transform = "rotateY(-20deg)";
-//     setTimeout(() => {
-//         input.style.transform = "rotateY(0deg)";
-//     }, 150);
-//     number.innerHTML = value
-// }
-
-
-
-
-
 let input = document.querySelector(".number-input");
-// const min = input.getAttribute("min");
-// const max = input.getAttribute("max");
+const min = input.getAttribute("min");
+const max = input.getAttribute("max");
 const step = Number(input.getAttribute("step") || 1);
 var value = Number(input.getAttribute("value") || 0);
 let number = document.querySelector(".number");
 number.innerHTML = value;
-
-let numberDisplay = document.getElementById("lengthValue");
-let password1El = document.getElementById("password1El");
-let password2El = document.getElementById("password2El");
-
-const min = 5;
-const max = 20;
-let passwordLength = 15;
-
-
-
 
 function increase() {
     if(max) {
@@ -95,7 +42,6 @@ function decrease() {
     }, 150);
     number.innerHTML = value
 }
-
 
 
 
